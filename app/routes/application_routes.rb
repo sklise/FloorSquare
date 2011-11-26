9c94583f8a1e136ab8030f866f3de99ba2a6d0e6
Steven Klise <steven@stevenklise.com>
2011-11-25 22:56:23 -0500

    Defining admin routes.

--- app/routes/application_routes.rb
+++ app/routes/application_routes.rb
@@ -2,6 +2,54 @@ get '/' do
   erb :front
 end
 
+#   ADMIN
+#---------------------------------------
+
+get '/admin/' do
+end
+
+get '/admin/apps' do
+end
+
+get '/admin/apps/new' do
+end
+
+post '/admin/apps/new' do
+end
+
+get '/admin/apps/:id/' do
+end
+
+get '/admin/apps/:id/edit' do
+end
+
+put '/admin/apps/:id' do
+end
+
+delete '/admin/apps/:id' do
+end
+
+get '/admin/devices' do
+end
+
+get '/admin/devices/new' do
+end
+
+post '/admin/devices/new' do
+end
+
+get '/admin/devices/:id' do
+end
+
+get '/admin/devices/:id/edit' do
+end
+
+put '/admin/devices/:id' do
+end
+
+delete '/admin/devices/:id' do
+end
+
 #   SASS
 #---------------------------------------
 
