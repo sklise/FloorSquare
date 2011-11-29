9b645d8550004710f4a96b249a9d78a091bdc13c
Steven Klise <steven@stevenklise.com>
2011-11-28 22:07:43 -0500

    Add APIKey properties to App and Device

--- app/models/app.rb
+++ app/models/app.rb
@@ -2,9 +2,9 @@ class App
   include DataMapper::Resource
 
   property :id,     Serial, :key => true
+  property :auth_key, APIKey
   property :email,  String
   property :url,    String
-  property :auth_key, APIKey
 
   has n, :swipes
 end
--- app/models/device.rb
+++ app/models/device.rb
@@ -2,7 +2,7 @@ class Device
   include DataMapper::Resource
 
   property :id, Serial, :key => true
-  property :auth_key, APIKey
+  property :auth_key, APIKey, :key => true
   property :location, String
   property :device_type_id, Integer
 
