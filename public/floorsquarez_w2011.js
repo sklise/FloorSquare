e78419a8aadd204d39848042132e186b7b2197a5
Steven Klise <steven@stevenklise.com>
2011-12-17 17:33:29 -0500

    Projects page

--- public/floorsquarez_w2011.js
+++ public/floorsquarez_w2011.js
@@ -105,7 +105,7 @@ var app = Sammy('#contentMain', function() {
   }); // end get #/
 
     this.get('#/checkins', function(context) {
-        $('#contentMain').empty()
+        $('#contentMain').empty();
         context.render('checkintable.mustache').appendTo("#contentMain");
         this.send($.getJSON, url+"/swipes", { app_key: "2d92b4126baeffefdbdd90f03c571963", since: "2011-12-16"})
         .then(function(json) {
@@ -129,13 +129,39 @@ var app = Sammy('#contentMain', function() {
         })
     });
 
+    this.get('#/projects/:nnumber', function(context) {
+        $('#contentMain').empty();
+        var nnumber = this.params['nnumber'];
+        $.ajax({
+            url: 'http://l:9292/floorsquare/members/'+nnumber+'/single?app_key=2d92b4126baeffefdbdd90f03c571963',
+            dataType: 'json',
+        })
+        .then(function(user) {
+            context.render('projectsView.mustache',user).appendTo('#contentMain');
+            console.log(user.netid);
+            var netid = user.netid;
+            $.ajax({
+             url: 'http://itp.nyu.edu/~mah593/projects_db_work/serving_scripts/get_projects_venue.php?venueid=84&netid='+netid,
+             dataType: 'jsonp',
+             success: function(data){
+             }
+            }).then(function(data) {
+
+                var mustache_magic_list = new Object();
+                var projectinfos = [];
+                for(i in data.results){
+                    console.log(data.results[i]);
+                    projectinfos.push({"title":data.results[i][1], "abstract":data.results[i][2]})
+                }
+                context.renderEach('projects.mustache',projectinfos).appendTo("#projects");
+            });
+        })
+    });
+
   this.get('#/checkin/:nnumber', function(context) {
-   
    var nnumber=this.params['nnumber'];
    var swipeid = null;
    $("#allStudentsBoard").empty().isotope('destroy').hide();
-
-    console.log("yo");
 
     this.send($.ajax, url+"/swipes/new", {
         data: { user_nnumber: nnumber, app_id: '4', device_id: '1', app_key: "2d92b4126baeffefdbdd90f03c571963",  extra: {checkin: true, }},
@@ -143,10 +169,11 @@ var app = Sammy('#contentMain', function() {
         crossDomain: true,
         success: function(data){
             console.log('data',data);
-
+            $('#contentMain').empty();
             
             // extra.app_id_4.skills= extra.app_id_4.skills.split(",");
             swipeid=data.swipeid;
+
             context.render('swipe.mustache', data).appendTo('#contentMain')
             .then(function(){
               // console.log('hii');
@@ -230,7 +257,7 @@ var fadeUnavail = function(){
     $(studentImages).each(function(index) {
         if ($(this).hasClass('unavail')) {
           // console.log($(this).parent('li'));
-            $(this).parent('li').css({'background-color' : 'rgba(20,20,20,0.8)', 'opacity' : '0.5 !important', 'color' : '#aaa'});
+            // $(this).parent('li').css({'background-color' : 'rgba(20,20,20,0.8)', 'opacity' : '0.5 !important', 'color' : '#aaa'});
         }
      });
 };
